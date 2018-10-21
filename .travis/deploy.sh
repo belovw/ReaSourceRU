f8ac74f86b92da217f661a6cf20ae778c0a6d884
mpl <m.pilyavskiy@gmail.com>
2018-10-21 16:48:12 +0300

    утвштпы

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,23 +1,15 @@
-#!/bin/sh
-
-set -e
-
-cd "$(dirname "$0")"
-
-echo $DEPLOY_KEY_PASSPHRASE | gpg --passphrase-fd 0 deploy_key.gpg
-
-eval "$(ssh-agent -s)"
-chmod 600 deploy_key
-ssh-add deploy_key
-
-git config push.default simple
-git config user.name 'ReaTeam Bot'
-git config user.email 'reateam-bot@cfillion.tk'
-git remote add deploy 'git@github.com:ReaScriptsRU/ReaScriptsRU.git'
-
-git fetch --unshallow || true
-git checkout "$TRAVIS_BRANCH"
-
-rvm $TRAVIS_RUBY_VERSION do reapack-index --commit
-
-git push deploy "$TRAVIS_BRANCH"
+#!/bin/sh\n
+set -e\n
+cd "$(dirname "$0")"\n
+echo $DEPLOY_KEY_PASSPHRASE | gpg --passphrase-fd 0 deploy_key.gpg\n
+eval "$(ssh-agent -s)"\n
+chmod 600 deploy_key\n
+ssh-add deploy_key\n
+git config push.default simple\n
+git config user.name 'ReaTeam Bot'\n
+git config user.email 'reateam-bot@cfillion.tk'\n
+git remote add deploy 'git@github.com:ReaScriptsRU/ReaScriptsRU.git'\n
+git fetch --unshallow || true\n
+git checkout "$TRAVIS_BRANCH"\n
+rvm $TRAVIS_RUBY_VERSION do reapack-index --commit\n
+git push deploy "$TRAVIS_BRANCH"\n
