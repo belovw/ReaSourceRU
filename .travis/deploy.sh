02c47d353536a03c1786754e3faa949175f193d2
cfillion <cfillion@users.noreply.github.com>
2018-10-21 10:10:16 -0400

    convert deploy.sh to Unix line endings

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
+
 set -e
-cd "$(dirname "$0")"echo $DEPLOY_KEY_PASSPHRASE | gpg --passphrase-fd 0 deploy_key.gpg
+
+cd "$(dirname "$0")"
+
+echo $DEPLOY_KEY_PASSPHRASE | gpg --passphrase-fd 0 deploy_key.gpg
+
 eval "$(ssh-agent -s)"
 chmod 600 deploy_key
 ssh-add deploy_key
+
 git config push.default simple
-git config user.name 'ReaTeam Bot'
-git config user.email 'reateam-bot@cfillion.tk'
+git config user.name 'ReaScriptsRU Bot'
+git config user.email 'reascriptsru-bot@mpl'
 git remote add deploy 'git@github.com:ReaScriptsRU/ReaScriptsRU.git'
+
 git fetch --unshallow || true
 git checkout "$TRAVIS_BRANCH"
+git pull
+
 rvm $TRAVIS_RUBY_VERSION do reapack-index --commit
+
 git push deploy "$TRAVIS_BRANCH"
