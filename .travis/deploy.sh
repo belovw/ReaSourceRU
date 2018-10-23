41e3d113c27d1c89418bf9eb65c4ea104935d4e8
mpl <m.pilyavskiy@gmail.com>
2018-10-23 08:36:36 +0300

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,7 +12,7 @@ ssh-add deploy_key
 
 git config push.default simple
 git config user.name 'ReaSourceRU Bot'
-git config user.email 'reasourceru-bot@cfillion.ca'
+git config user.email 'reasourceru@gmail.com'
 git remote add deploy 'git@github.com:ReaSourceRU/ReaSourceRU.git'
 
 git fetch --unshallow || true
